b56eddaa4ab736f746feee14f7fd3692ea6237e9
Bhomik Tomar <bhomiktomar@gmail.com>
2025-04-09 01:20:31 +0530

    error handling

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -9,6 +10,7 @@ const tourSchema = new mongoose.Schema({
         trim: true,
         maxLength: [40, 'A tour must have less than or equals to 40 characters'],
         minLength: [10, 'A tour must have more than or equals to 10 characters']
+        // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
     slug: String,
     duration: {
@@ -23,7 +25,7 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, 'A tour must have a difficulty'],
         enum: {
-            value: ['easy', 'medium', 'difficult'],
+            values: ['easy', 'medium', 'difficult'],
             message: 'Difficulty is either: easy, medium, difficult'
         }
     },
@@ -41,7 +43,15 @@ const tourSchema = new mongoose.Schema({
         type: Number,
         required: [true, 'A tour must have a price']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+        type: Number,
+        validate: {
+            validator: function(val){
+                return val < this.price;
+            },
+            message: 'Discount price ({VALUE}) should be less than the price'
+        }
+    },
     summary: {
         type: String,
         trim: true,
