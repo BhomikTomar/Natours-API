97a2016ec6f1e7363a5b696dc9a87b9e25773fe4
Bhomik Tomar <bhomiktomar@gmail.com>
2025-04-16 16:58:22 +0530

    implemented factory handlers and restricting the routes

--- natours/controllers/userController.js
+++ natours/controllers/userController.js
@@ -11,33 +11,26 @@ const filterObj = (obj, ...allowedFields) => {
     });
 
     return newObj;
+};
+
+exports.getMe = (req, res, next) => {
+    req.params.id = req.user.id;
+    next();
 }
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users
-    }
-  });
-});
 
 exports.updateMe = catchAsync(async(req, res, next) => {
     if(req.body.password || req.body.passwordConfirm){
         return next(new AppError('This route is not for password updates. Please use /updateMyPassword', 400));
     }
-
+    
     const filteredBody = filterObj(req.body, 'name', 'email');
-
+    
     const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
         new: true,
         runValidators: true
     });
-
+    
     res.status(200).json({
         status: 'success',
         data: {
@@ -48,19 +41,12 @@ exports.updateMe = catchAsync(async(req, res, next) => {
 
 exports.deleteMe = catchAsync(async (req, res, next) => {
     await User.findByIdAndUpdate(req.user.id, { active: false });
-
+    
     res.status(204).json({
         status: 'success',
         data: null
     })
 });
-
-exports.getUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined'
-    });
-};
 
 exports.createUser = (req, res) => {
     res.status(500).json({
@@ -69,11 +55,7 @@ exports.createUser = (req, res) => {
     });
 };
 
-exports.updateUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined'
-    });
-};
-
+exports.getAllUsers = factory.getAll(User);
+exports.getUser = factory.getOne(User);
+exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
--- natours/routes/userRoutes.js
+++ natours/routes/userRoutes.js
@@ -3,39 +3,19 @@ const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
 const router = express.Router();
 
-router
-    .route('/signup')
-    .post(authController.signup);
+router.route('/signup').post(authController.signup);
+router.route('/login').post(authController.login);
+router.route('/forgotPassword').post(authController.forgotPassword);
+router.route('/resetPassword/:token').patch(authController.resetPassword);
 
-router
-    .route('/login')
-    .post(authController.login);
+router.use(authController.protect);
 
-router
-    .route('/forgotPassword')
-    .post(authController.forgotPassword);
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
-router
-    .route('/resetPassword/:token')
-    .patch(authController.resetPassword);
-
-router.patch(
-    '/updateMyPassword',
-    authController.protect,
-    authController.updatePassword
-)
-
-router.patch(
-    '/updateMe',
-    authController.protect,
-    userController.updateMe
-)
-
-router.delete(
-    '/deleteMe',
-    authController.protect,
-    userController.deleteMe
-)
+router.use(authController.restrictTo('admin'));
 
 router
     .route('/')
