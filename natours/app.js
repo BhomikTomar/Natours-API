e1408bbe0666dd1d8c4bdc566b7b445d57a9a656
Bhomik Tomar <bhomiktomar@gmail.com>
2025-04-19 17:14:31 +0530

    added login page

--- natours/app.js
+++ natours/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -23,17 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // global middlewares
-app.use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "https://apis.google.com"],
-        objectSrc: ["'none'"],
-        upgradeInsecureRequests: [],
-      },
-    })
-  );
-  
+app.use(helmet());
 
 //development logging
 if(process.env.NODE_ENV === 'development'){
@@ -50,6 +41,7 @@ app.use('/api', limiter);
 
 //body parser
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 //data sanitisation against nosql injection
 app.use(mongoSanitize());
--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -73,6 +73,8 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if( req.cookies.jwt){
+    token = req.cookies.jwt;
   }
 
   if (!token) {
@@ -104,6 +106,33 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   // GRANT ACCESS TO PROTECTED ROUTE
   req.user = currentUser;
+  next();
+});
+
+//only for rendered pages
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  if( req.cookies.jwt){
+    // verify token
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt, 
+      process.env.JWT_SECRET
+    );
+    
+    // Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next();
+    }
+
+    // Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next();
+    }
+    
+    // there is a logged in user
+    res.locals.user = currentUser;
+    next();
+  }
   next();
 });
 
