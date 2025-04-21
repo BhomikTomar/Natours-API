fbc43474341ca56399876ddcbb71eb3c2b16a46d
Bhomik Tomar <bhomiktomar@gmail.com>
2025-04-21 23:34:01 +0530

    updating user data

--- natours/public/js/bundle.js
+++ natours/public/js/bundle.js
@@ -12977,7 +12977,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64260" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58500" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
