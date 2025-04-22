3a2c629ec666d40b1a1b9fe759292fdeb426341b
Bhomik Tomar <bhomiktomar@gmail.com>
2025-04-22 17:50:36 +0530

    multi images uploader

--- natours/controllers/userController.js
+++ natours/controllers/userController.js
@@ -1,9 +1,9 @@
 const User = require('../models/userModel');
 const multer = require('multer');
+const sharp = require('sharp');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handleFactory');
-const sharp = require('sharp');
 
 // const multerStorage = multer.diskStorage({
 //     destination: (req, file, cb) =>{
@@ -32,19 +32,19 @@ const upload = multer({
 
 exports.uploadUserPhoto = upload.single('photo');
 
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
     if(!req.file) return next();
 
     req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-    sharp(req.file.buffer)
+    await sharp(req.file.buffer)
         .resize(500, 500)
         .toFormat('jpeg')
         .jpeg({ quality: 90 })
         .toFile(`public/img/users/${req.file.filename}`);
 
     next();
-};
+});
 
 const filterObj = (obj, ...allowedFields) => {
     const newObj = {};
